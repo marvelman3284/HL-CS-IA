5cfa1938f716ff99dad7ea10b7140bc66a79a06e
marvelman3284 <jedimaster3284@gmail.com>
2023-02-13 13:25:48 -0500

    final product w/ comments

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,6 @@
 import sympy as sp
 from sympy.abc import t
+from typing import Any
 
 
 def clean_equation(eq: str):
@@ -15,49 +16,58 @@ def clean_equation(eq: str):
         Any: a sympify equation
     """
 
+    # DOC: if not passed an equation then take an input
     if eq == "":
         eq = input("Enter an equation: ")
 
     try:
         return sp.sympify(eq)
-    except ValueError:
+    except ValueError:  # DOC: if a ValuError occurs then the equation is not formatted correctly
         print("Incorrect syntax!")
         print(
             "Enter one side of the equation (set everything equal to 0) using x and y (eg: x**2 - y == x**2 = y)"
         )
         new_eq: str = input("Enter the equation with correct syntax: ")
-        return clean_equation(new_eq)
+        return clean_equation(
+            new_eq
+        )  # DOC: take a new input and call again recursively
 
 
+# DOC: define a 2-d point class
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    # DOC: __repr__ needed in order to print to console (equivalent to the toString override method in java)
     def __repr__(self) -> str:
         return f"({self.x}, {self.y})"
 
+    # DOC: __eq__ is used for checking equality
     def __eq__(self, o: object) -> bool:
         return self.x == o.x and self.y == o.y if isinstance(o, Point) else False
 
     #  DOC: __key and __hash__ needed to be able to call `set()` on the Point class
-    def __key(self):
+    def __key(self) -> tuple[Any]:
         return (self.x, self.y)
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self.__key())
 
 
+# DOC: define a custom parametric equation class to be used as a custom type
 class Parametric:
-    def __init__(self, x_eq: str, y_eq: str):
+    # DOC: initalize values and clean the inputted data
+    def __init__(self, x_eq: str, y_eq: str) -> None:
         self.x_eq = clean_equation(x_eq)
         self.y_eq = clean_equation(y_eq)
 
-    def swap(self, letter: str):
-        # DOC: plug in a letter for t
+    # DOC: swap a different letter variable for t
+    def swap(self, letter: str) -> None:
         letter = sp.symbols(letter)
         self.x_eq = self.x_eq.subs(t, letter)
         self.y_eq = self.y_eq.subs(t, letter)
 
+    # DOC: __repr__ needed for printing to console
     def __repr__(self) -> str:
         return f"({self.x_eq}, {self.y_eq})"
--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
-import sympy as sp
-import logging
-from rich import print
-from sympy.abc import x, y
-from helpers import Parametric, Point
+import sympy as sp  # DOC: math operations
+from sympy.abc import x, y  # DOC: specific math imports
+import logging  # DOC: import built-in logging function
+from rich import print  # DOC: pretty print
+from helpers import Parametric, Point  # DOC: import classes from a local file
 
+
+#  DOC: setup the logger
 logging.basicConfig(filename="logger.log", filemode="w", level=logging.DEBUG)
 
 
 def main(parametric_eq1: Parametric, parametric_eq2: Parametric):
     intersections: list[Point] = []
-    dual_intersections = {
-        "equation1": [],
-        "equation2": []
-    }
+    dual_intersections = {"equation1": [], "equation2": []}
 
     # DOC: swap t for x and y in parametric equation 1 and 2, respectively
     parametric_eq1.swap("x")
@@ -32,25 +31,32 @@ def main(parametric_eq1: Parametric, parametric_eq2: Parametric):
     logging.debug(f'put eq3 in terms of x(a): "x="{a}')
 
     for i in a:
-        b = eq4.subs(x, i)  # DOC: substitutes a[0] for y in eq4
+        b = eq4.subs(x, i)  # DOC: substitutes `a[i]` for y in eq4
         logging.debug(f"substitue x into eq4(b): {b}")
 
         c = sp.solve(b, y)  # DOC: solves `b` for y
         logging.debug(f"solves eq4 for y(c): {c}")
 
         for j in c:
-            d = sp.solve(eq3.subs(y, j), x)  # DOC: solves equation 3 with c[0]
+            d = sp.solve(eq3.subs(y, j), x)  # DOC: solves `eq3` with `c[i]`
             logging.debug(f"plug that solution back into eq 3(d): {d}")
 
             for i in d:
-                e = Point(parametric_eq1.x_eq.subs(x, i), parametric_eq1.y_eq.subs(x, i))
+                e = Point(
+                    parametric_eq1.x_eq.subs(x, i), parametric_eq1.y_eq.subs(x, i)
+                )
                 if "I" in str(e):
-                    continue
+                    # NOTE: we don't really care about imaginary intersections, but log them just in case
+                    logging.info(f"Imaginary intersection: {e}")
                 else:
+                    # DOC: if it's real then append it to the list of intersections
                     dual_intersections["equation1"].append(e)
 
             for i in c:
-                e = Point(parametric_eq2.x_eq.subs(y, i), parametric_eq2.y_eq.subs(y, i))
+                # DOC: repeat the same process as above, just with the second parametric equation
+                e = Point(
+                    parametric_eq2.x_eq.subs(y, i), parametric_eq2.y_eq.subs(y, i)
+                )
                 if "I" in str(e):
                     continue
                 else:
@@ -62,13 +68,12 @@ def main(parametric_eq1: Parametric, parametric_eq2: Parametric):
 
     for i in dual_intersections["equation1"]:
         if i in dual_intersections["equation2"]:
-            intersections.append(i)
+            intersections.append(i)  # DOC: combine both lists
 
-    return [*set(intersections)]
+    return [*set(intersections)]  # DOC: transform into a set in order to remove duplicates and then return
 
 
 if __name__ == "__main__":
-
     # NOTE: working: polynomial, radical, rational
     # FIX: not working: trig function,  exponential, rational to an exponent, abs?
     para = Parametric("t**(1/2)", "t")
